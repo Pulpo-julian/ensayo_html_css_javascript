11b2de5454e76cf76baa8ce0015531a5ad2818a4
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-21 18:13:00 -0500

    cambio parseint en variables

--- script/main.js
+++ script/main.js
@@ -1,8 +1,10 @@
 var nombre = prompt("como te llamas?");
 alert("hola !!...  como estas " + nombre + "?");
 
-var a = parseInt(prompt("inserta un numero"));
-var b = parseInt(prompt("Ahora inserta otro numero :)"));
+var a = prompt("inserta un numero");
+parseInt(a);
+var b = prompt("Ahora inserta otro numero :)");
+parseInt(b);
 var c = a + b;
 
 alert(" la suma de los numeros es : " + c);
