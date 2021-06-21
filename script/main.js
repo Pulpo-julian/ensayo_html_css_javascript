6c6623b4803cd8253abf93d2532ab20bd18ca975
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-21 14:48:29 -0500

    añado todo en cada archivo

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
 var nombre = prompt("como te llamas?");
-alert("hola como estas " + nombre);
+alert("hola !!...  como estas " + nombre + "?");
+
+var a = parseInt(prompt("inserta un numero"));
+var b = parseInt(prompt("Ahora inserta otro numero :)"));
+var c = a + b;
+
+alert(" la suma de los numeros es : " + c);
 
