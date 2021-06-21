12f014b286020071073a98d29d3c11d2730387d3
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-21 18:17:14 -0500

    cambio otra vez el parseint dentro dela variable

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
 var nombre = prompt("como te llamas?");
 alert("hola !!...  como estas " + nombre + "?");
 
-var a = prompt("inserta un numero");
-parseInt(a);
-var b = prompt("Ahora inserta otro numero :)");
-parseInt(b);
+var a = parseInt(prompt("inserta un numero"));
+
+var b = parseInt(prompt("Ahora inserta otro numero :)"));
+
 var c = a + b;
 
 alert(" la suma de los numeros es : " + c);
